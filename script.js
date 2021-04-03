fe3c387562fef49c3bf2e426bd71af1942ea6db6
Ложкин Егор Денисович <pl8389@pl1.edu>
2021-04-03 09:56:48 +0500

    nujbukyhk

--- script.js
+++ script.js
@@ -2,34 +2,50 @@ document.onkeydown = checkButton;
 let cube = {
     x: 0,
     y: 0,
-    id: 'main'
+    id: 'cube',
+    width: 56,
+    height: 56,
+    trn: .25,
+    step: 100,
+    color: 'black',
 }
-function checkButton(e){
-   let k = e || window.event;
-   console.log(k);
-   if (k.keyCode === 38){
+let map = {
+    width: 900,
+    height: 900
+}
+function checkButton(event){
+   if (event.keyCode === 38){
        // key: "ArrowUp"
-       cube.y -= 4;
-       render(cube.id, cube.x, cube.y);
+       if (cube.y > 0){
+       cube.y -= cube.step;
+       }
    }
-   else if (k.keyCode === 40){
-       // key: "ArrowDown" 
-       cube.y += 4;
-       render(cube.id, cube.x, cube.y);
+    if (event.keyCode === 40){
+       // key: "ArrowDown"
+       if (cube.y < map.height){ 
+       cube.y += cube.step;
+       }
    }
-   else if (k.keyCode === 37){
+    if (event.keyCode === 37){
        // key: "ArrowLeft"
-       cube.x -= 4;
-       render(cube.id, cube.x, cube.y);
+       if (cube.x > 0){
+       cube.x -= cube.step;
+       }
    }
-   else if (k.keyCode === 39){
+    if (event.keyCode === 39){
        // key: "ArrowRight"
-       cube.x += 4;
-       render(cube.id, cube.x, cube.y);
+       if (cube.x < map.width){
+       cube.x += cube.step;
+       }
    }
+   render(cube);
 }   
-function render(id, x, y){
-    document.getElementById(id).style.top = y + 'px';
-    document.getElementById(id).style.left = x + 'px';
+function render(cube){
+    document.getElementById(cube.id).style.top = cube.y + 'px';
+    document.getElementById(cube.id).style.left = cube.x + 'px';
+    document.getElementById(cube.id).style.transition = 'ease ' + cube.trn + 's';
+    document.getElementById(cube.id).style.width = cube.width + 'px';
+    document.getElementById(cube.id).style.height = cube.height + 'px';
+    document.getElementById(cube.id).style.backgroundColor = cube.color;
 }
-render(cube.id, cube.x, cube.y);
+render(cube);
