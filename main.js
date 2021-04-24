f7c85d07642b09ca7116ab566b3625286448a43e
Ложкин Егор Денисович <pl8389@pl1.edu>
2021-04-24 09:57:37 +0500

    bvjjuygj

--- main.js
+++ main.js
@@ -1,20 +1,55 @@
+let map = {
+    x: 0,
+    y: 0,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    color: '#f5f5f5'
+}
+
 let cube = {
-    x: 300,
-    y: 300,
+    x: 0,
+    y: 0,
     id: 'cube',
     width: 56,
     height: 56,
     trn: .25,
     step: 56,
-    color: 'black',
-}
-let map = {
-    x: 100,
-    y: 100,
-    width: 470,
-    height: 470,
-    color: '#f5f5f5'
+    color: 'black'
 }
 
+let bots = [];
+
+let bot = {
+    x: 0,
+    y: 0,
+    id: 1,
+    width: 56,
+    height: 56,
+    trn: .25,
+    step: 56,
+    color: 'red'
+}
+
+// bots.push(spawnCube(map, bot));
+
+let buffs = [];
+
+let buff = {
+    x: 0,
+    y: 0,
+    id: 1,
+    width: 56,
+    height: 56,
+    trn: .25,
+    step: 56,
+    color: 'green'
+}
+
+// buffs.push(spawnCube(map, buff));
+
 renderMap(map);
-renderCube(cube);
+renderCube(spawnCube( map, cube));
+// renderBots(bots);
+// renderBuffs(buffs);
+
+let tike = setInterval(timeTike, 1000);
